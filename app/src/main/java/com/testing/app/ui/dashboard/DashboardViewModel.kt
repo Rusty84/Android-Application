932116e87782896c43cde824da75817ebed7bcd2
Morley, Russell <Russell.Morley@experian.com>
2020-07-06 17:16:27 +0100

    Initial commit

--- app/src/main/java/com/testing/app/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/testing/app/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is the dashboard"
+        value = "This is the dashboard screen"
     }
     val text: LiveData<String> = _text
 }
